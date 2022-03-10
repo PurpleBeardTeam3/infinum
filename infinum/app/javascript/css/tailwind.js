7f9880391ceb3da2060452de40e1fc366bab9250
AlexUkLenovoYoga <alcatraz1987z@yahoo.com>
2022-03-10 16:54:00 +0100

    Session and Authentication INF-43 Fixes #16

---
+++ infinum/app/javascript/css/tailwind.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+        default: "#60b0e2",
+        "500": "#41a1dd",
+        }
+      },
+      spacing: {
+          '72': '18rem',
+          '84': '21rem',
+          '96': '24rem',
+      }
+      },
+  },
+  variants: {},
+  plugins: [],
+}
